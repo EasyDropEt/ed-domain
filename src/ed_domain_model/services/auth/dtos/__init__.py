9e0f0ebc3c1235f7391d32cb55119d8669fb2426
ffekirnew <ffekirnew0808@gmail.com>
2025-03-14 18:31:53 +0300

    Add common platforms

--- src/ed_domain_model/services/auth/dtos/__init__.py
+++ src/ed_domain_model/services/auth/dtos/__init__.py
@@ -1,15 +1,13 @@
-from src.ed_domain_model.services.auth.dtos.create_user_dto import \
-    CreateUserDto
-from src.ed_domain_model.services.auth.dtos.create_user_verify_dto import \
+from ed_domain_model.services.auth.dtos.create_user_dto import CreateUserDto
+from ed_domain_model.services.auth.dtos.create_user_verify_dto import \
     CreateUserVerifyDto
-from src.ed_domain_model.services.auth.dtos.login_user_dto import LoginUserDto
-from src.ed_domain_model.services.auth.dtos.login_user_verify_dto import \
+from ed_domain_model.services.auth.dtos.login_user_dto import LoginUserDto
+from ed_domain_model.services.auth.dtos.login_user_verify_dto import \
     LoginUserVerifyDto
-from src.ed_domain_model.services.auth.dtos.unverified_user_dto import \
+from ed_domain_model.services.auth.dtos.unverified_user_dto import \
     UnverifiedUserDto
-from src.ed_domain_model.services.auth.dtos.user_dto import UserDto
-from src.ed_domain_model.services.auth.dtos.verify_token_dto import \
-    VerifyTokenDto
+from ed_domain_model.services.auth.dtos.user_dto import UserDto
+from ed_domain_model.services.auth.dtos.verify_token_dto import VerifyTokenDto
 
 __all__ = [
     "CreateUserDto",
