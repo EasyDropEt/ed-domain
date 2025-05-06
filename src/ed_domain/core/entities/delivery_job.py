b3d09f5de0bfc85d4d7ebf120761d0784643d0b2
ffekirnew <ffekirnew0808@gmail.com>
2025-05-06 23:31:08 +0300

    Fix delivery job entity

--- src/ed_domain/core/entities/delivery_job.py
+++ src/ed_domain/core/entities/delivery_job.py
@@ -1,9 +1,21 @@
 from datetime import datetime
 from enum import StrEnum
-from typing import NotRequired
+from typing import NotRequired, TypedDict
 from uuid import UUID
 
 from ed_domain.core.entities.base_entity import BaseEntity
+
+
+class WayPointType(StrEnum):
+    PICKUP = "PICKUP"
+    DROP_OFF = "DROP_OFF"
+
+
+class WayPoint(TypedDict):
+    order_id: UUID
+    type: WayPointType
+    eta: datetime
+    sequence: int
 
 
 class DeliveryJobStatus(StrEnum):
@@ -15,9 +27,11 @@ class DeliveryJobStatus(StrEnum):
 
 
 class DeliveryJob(BaseEntity):
-    route_id: UUID
-    driver_id: NotRequired[UUID]
-    driver_payment_id: NotRequired[UUID]
+    waypoints: list[WayPoint]
     status: DeliveryJobStatus
     estimated_payment: float
     estimated_completion_time: datetime
+    estimated_distance_in_kms: float
+    estimated_time_in_minutes: int
+    driver_id: NotRequired[UUID]
+    driver_payment_id: NotRequired[UUID]
