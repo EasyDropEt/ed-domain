769abfa7b414ad72692d625a939b89f969ba807b
ffekirnew <ffekirnew0808@gmail.com>
2025-05-11 11:30:48 +0300

    Add pickup & dropoff actions to otps

--- src/ed_domain/core/entities/otp.py
+++ src/ed_domain/core/entities/otp.py
@@ -9,6 +9,8 @@ class OtpVerificationAction(StrEnum):
     VERIFY_EMAIL = "VERIFY_EMAIL"
     VERIFY_PHONE_NUMBER = "VERIFY_PHONE_NUMBER"
     LOGIN = "LOGIN"
+    PICK_UP = "PICK_UP"
+    DROP_OFF = "DROP_OFF"
 
 
 class Otp(BaseEntity):
