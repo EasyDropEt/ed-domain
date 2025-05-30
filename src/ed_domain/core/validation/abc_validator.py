64f9002d0ce1bcfbfabe320e307f9d53a8be8491
ffekirnew <ffekirnew0808@gmail.com>
2025-05-30 08:54:09 +0300

    fix: Minor changes

--- src/ed_domain/core/validation/abc_validator.py
+++ src/ed_domain/core/validation/abc_validator.py
@@ -9,9 +9,10 @@ T = TypeVar("T")
 class ABCValidator(Generic[T], ABC):
     DEFAULT_ERROR_LOCATION: str = "body"
 
-    def __init__(self):
+    def __init__(self, location: str = DEFAULT_ERROR_LOCATION):
         self._is_valid: bool = False
         self._errors: list[ValidationError] = []
+        self._location: str = location
 
     @property
     def is_valid(self) -> bool:
@@ -21,8 +22,16 @@ class ABCValidator(Generic[T], ABC):
     def errors(self) -> Optional[list[ValidationError]]:
         return self._errors
 
+    def validate_many(
+        self,
+        values: list[T],
+    ) -> None:
+        for index, value in enumerate(values):
+            self.validate(value, f"{self._location}:{index + 1}")
+
     @abstractmethod
     def validate(
         self,
-        value: T | list[T],
+        value: T,
+        location: Optional[str] = None,
     ) -> None: ...
