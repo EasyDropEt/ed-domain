90f104e29187279cbe8185e959aa7aaab33c2267
ffekirnew <ffekirnew0808@gmail.com>
2025-06-04 15:35:49 +0300

    refactoring & create aggregate roots

--- src/ed_domain/core/entities/notification.py
+++ src/ed_domain/core/entities/notification.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import StrEnum
 from uuid import UUID
 
@@ -10,8 +11,31 @@ class NotificationType(StrEnum):
     IN_APP = "IN_APP"
 
 
+@dataclass
 class Notification(BaseEntity):
     user_id: UUID
     notification_type: NotificationType
     message: str
     read_status: bool
+
+    def to_dict(self) -> dict:
+        base_dict = super().to_dict()
+        return {
+            **base_dict,
+            "user_id": str(self.user_id),
+            "notification_type": self.notification_type.value,
+            "message": self.message,
+            "read_status": self.read_status,
+        }
+
+    @classmethod
+    def from_dict(cls, dict_value: dict) -> "Notification":
+        base_entity = BaseEntity.from_dict(dict_value)
+        return cls(
+            **vars(base_entity),
+            user_id=UUID(dict_value["user_id"]),
+            notification_type=NotificationType(
+                dict_value["notification_type"]),
+            message=dict_value["message"],
+            read_status=dict_value["read_status"],
+        )
--- src/ed_domain/core/value_objects/money.py
+++ src/ed_domain/core/value_objects/money.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 from enum import StrEnum
-from typing import TypedDict
+
+from ed_domain.core.value_objects.base_value_object import BaseValueObject
 
 
 class Currency(StrEnum):
@@ -7,6 +9,25 @@ class Currency(StrEnum):
     USD = "usd"
 
 
-class Money(TypedDict):
-    amount: float
+@dataclass
+class Money(BaseValueObject):
+    value: float
     currency: Currency
+
+    def to_dict(self) -> dict:
+        base_dict = super().to_dict()
+        return {
+            **base_dict,
+            "value": self.value,
+            "currency": self.currency.value,
+        }
+
+    @classmethod
+    def from_dict(cls, dict_value: dict) -> "Money":
+        base_value_object = BaseValueObject.from_dict(dict_value)
+
+        return cls(
+            **vars(base_value_object),
+            value=dict_value["value"],
+            currency=Currency(dict_value["currency"]),
+        )
