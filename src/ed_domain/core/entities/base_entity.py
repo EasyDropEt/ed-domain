c38c321a2840538ef17eebd8107a995c56813605
ffekirnew <ffekirnew0808@gmail.com>
2025-06-06 00:56:41 +0300

    feat: Add deleted at datetime

--- src/ed_domain/core/entities/base_entity.py
+++ src/ed_domain/core/entities/base_entity.py
@@ -9,6 +9,7 @@ class BaseEntity(BaseDomainObject):
     create_datetime: datetime
     update_datetime: datetime
     deleted: bool
+    deleted_datetime: datetime | None = None
 
     def to_dict(self) -> dict:
         base_dict = super().to_dict()
@@ -17,6 +18,9 @@ class BaseEntity(BaseDomainObject):
             "create_datetime": self.create_datetime.isoformat(),
             "update_datetime": self.update_datetime.isoformat(),
             "deleted": self.deleted,
+            "deleted_datetime": (
+                self.deleted_datetime.isoformat() if self.deleted_datetime else None
+            ),
         }
 
     @classmethod
@@ -29,4 +33,9 @@ class BaseEntity(BaseDomainObject):
             update_datetime=datetime.fromisoformat(
                 dict_value["update_datetime"]),
             deleted=dict_value["deleted"],
+            deleted_datetime=(
+                datetime.fromisoformat(dict_value["deleted_datetime"])
+                if dict_value.get("deleted_datetime")
+                else None
+            )
         )
