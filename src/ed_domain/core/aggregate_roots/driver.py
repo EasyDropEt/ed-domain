11cd1519f5a7c91774023f07190790466b89b096
ffekirnew <ffekirnew0808@gmail.com>
2025-06-06 18:05:24 +0300

    fix: Remove unwanted methods

--- src/ed_domain/core/aggregate_roots/driver.py
+++ src/ed_domain/core/aggregate_roots/driver.py
@@ -35,34 +35,3 @@ class Driver(BaseAggregateRoot):
 
     def update_phone_number(self, new_phone_number: str) -> None:
         self.phone_number = new_phone_number
-
-    def to_dict(self) -> dict:
-        base_dict = super().to_dict()
-        return {
-            **base_dict,
-            "user_id": self.user_id,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "profile_image": self.profile_image,
-            "phone_number": self.phone_number,
-            "residence_location": str(self.residence_location),
-            "car": str(self.car),
-            "current_location": str(self.current_location),
-            "email": self.email,
-        }
-
-    @classmethod
-    def from_dict(cls, dict_value: dict) -> "Driver":
-        base_entity = BaseAggregateRoot.from_dict(dict_value)
-        return cls(
-            **vars(base_entity),
-            user_id=dict_value["user_id"],
-            first_name=dict_value["first_name"],
-            last_name=dict_value["last_name"],
-            profile_image=dict_value["profile_image"],
-            phone_number=dict_value["phone_number"],
-            email=dict_value.get("email"),
-            residence_location=dict_value["location"],
-            car=dict_value["car"],
-            current_location=dict_value["current_location"],
-        )
