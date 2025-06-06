6249f928e395205165b14b48d9cfd62b94d081d7
ffekirnew <ffekirnew0808@gmail.com>
2025-06-06 19:39:44 +0300

    fix: Minor changes

--- src/ed_domain/validation/__init__.py
+++ src/ed_domain/validation/__init__.py
@@ -1,7 +1,7 @@
-from ed_domain.core.validation.abc_validator import ABCValidator
-from ed_domain.core.validation.validation_error import ValidationError
-from ed_domain.core.validation.validation_error_type import ValidationErrorType
-from ed_domain.core.validation.validation_response import ValidationResponse
+from ed_domain.validation.abc_validator import ABCValidator
+from ed_domain.validation.validation_error import ValidationError
+from ed_domain.validation.validation_error_type import ValidationErrorType
+from ed_domain.validation.validation_response import ValidationResponse
 
 __all__ = [
     "ABCValidator",
