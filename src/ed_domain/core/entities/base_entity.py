946f5b3c5645713a97ac7651893c9fd542b231f2
ffekirnew <ffekirnew0808@gmail.com>
2025-06-14 22:48:21 +0300

    fix: Minor changes

--- src/ed_domain/core/entities/base_entity.py
+++ src/ed_domain/core/entities/base_entity.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import UTC, datetime
 from typing import Optional
 
 from ed_domain.core.base_domain_object import BaseDomainObject
@@ -11,3 +11,7 @@ class BaseEntity(BaseDomainObject):
     update_datetime: datetime
     deleted: bool
     deleted_datetime: Optional[datetime]
+
+    def delete(self) -> None:
+        self.deleted = True
+        self.deleted_datetime = datetime.now(UTC)
